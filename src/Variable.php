7088530534b4a056917bf332e8ca63b8072b9b4b
John Charman <john.charman@imhotek.net>
2024-08-12 11:18:31 +0100

    Improve Variable->__toString()

--- src/Variable.php
+++ src/Variable.php
@@ -2,32 +2,33 @@
 
 namespace GraphQL;
 
-use BackedEnum;
-use Stringable;
+use GraphQL\Util\StringLiteralFormatter;
 
 final readonly class Variable implements \Stringable
 {
-    /**
-     * @param null|array<mixed>|scalar|BackedEnum|Stringable $defaultValue
-     */
     public function __construct(
         public string $name,
         public string $type,
         public bool $nonNullable = false,
-        public null|bool|float|int|string|array|Stringable|BackedEnum $defaultValue = null,
+        public mixed $defaultValue = null
     ) {
     }
 
     public function __toString(): string
     {
-        return sprintf(
-            '$%s: %s%s%s',
+        $varString = sprintf(
+            '$%s: %s%s',
             $this->name,
             $this->type,
             $this->nonNullable ? '!' : '',
-            isset($this->defaultValue) ?
-                sprintf('=%s', json_encode($this->defaultValue)) :
-                '',
         );
+
+        if (!isset($this->defaultValue)) {
+            return $varString;
+        }
+
+        return sprintf('%s=%s', $varString, is_array($this->defaultValue) ?
+            StringLiteralFormatter::formatArrayForGQLQuery($this->defaultValue) :
+            StringLiteralFormatter::formatValueForRHS($this->defaultValue));
     }
 }
